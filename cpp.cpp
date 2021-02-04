20a5a80cc5e87700b29156e4d62086f40c520932
rahi-droid <78537540+rahi-droid@users.noreply.github.com>
2021-02-04 17:56:41 +0600

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string f = "i am beerus";
+    
+    int a = 19;
+
+    
+    cout<< f<<endl; 
+    
+    cout<< "my age is : "<< a;
+
+
+
+    return 0;
+}
